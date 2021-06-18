bb97f7049979cbb689af842b8fbc197d591a902a
GabrielDiasDutra <gabriel.dutra@ezops.com.br>
2021-06-18 12:38:26 -0300

    Feat: Get Parameters

---
+++ getParameters/index.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const AWS = require('aws-sdk')
+const fs = require('fs')
+const throttledQueue = require('throttled-queue')
+
+const KeyAndValue = require('../helpers/KeyAndValue')
+
+const wait = throttledQueue(3, 1000) // at most 3 requests per second.
+
+const SSM = new AWS.SSM()
+
+console.log(`Profile: ${process.env.AWS_PROFILE}`)
+
+function insertParameterJSON(Name, Value) {
+  const parameter = `"${Name}": "${Value}",`
+  fs.appendFile('./getParameters/Value.json', parameter, (err) => {
+    if (err) {
+      throw err
+    }
+    console.log('Parameter + Value = saved.')
+  })
+}
+
+function getParameter(data) {
+  data.keys.map((value, index) => {
+    const params = {
+      Name: value,
+    }
+    wait(() => {
+      SSM.getParameter(params, (err, response) => {
+        if (err) console.log(err.code)
+        else {
+          console.log(`Name: ${value}`)
+          console.log(`Value: ${response.Parameter.Value}`)
+          insertParameterJSON(value, response.Parameter.Value)
+        }
+      })
+    })
+  })
+}
+
+fs.readFile('./getParameters/Parameters.json', 'utf8', (err, data) => {
+  if (err) console.error(err)
+  const dataJson = KeyAndValue(JSON.parse(data))
+  getParameter(dataJson)
+})
